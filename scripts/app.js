2b3836aff8fc859da15d647d5d3c5ebe14775cbb
Kaiky Guizelini Santos da Silva <kaikyguizelinisantosdasilva@gmail.com>
2021-11-29 23:03:00 -0300

    Placar de pontuação adicionado

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,9 @@
 var jogador = "X";
 var quantidadeJogadas = 0;
 var jogoFinalizado = false;
-
+var pontuacaoX = 0;
+var pontuacaoO = 0;
+var ganhou = true;
 function jogar(button) {
     if(jogoFinalizado) {
         return;
@@ -12,25 +14,34 @@ function jogar(button) {
 
     setTimeout(function() {
         quantidadeJogadas++;
-
+        ganhou = true;
         if(jogoFinalizado) {
-            window.alert(jogador + " é o vencedor!!!");
+            alert("Jogador " + jogador + " venceu!!!");
+            if(jogador === "X") {
+                pontuacaoX++;
+            } else {
+                pontuacaoO++;
+            }
+            ganhou = false;
+            resetar();
         } else if(quantidadeJogadas === 9) {
-            window.alert("Jogo empatado!!!");
+            alert("Jogo empatado!!!");
         } 
-
-        if(jogador === "X") {
-            jogador = "O";
-        } else {
-            jogador = "X";
+        
+        if(ganhou) {
+            if(jogador === "X") {
+                jogador = "O";
+            } else {
+                jogador = "X";
+            }
         }
     }, 0);
 }
 
 function validarJogo() {
-    const botoes = document.querySelectorAll(".tabuleiro .botao");
+    var botoes = document.querySelectorAll(".tabuleiro .botao");
 
-    const validacao = botoes[0].innerHTML !== "&nbsp;" && botoes[0].innerHTML === botoes[1].innerHTML && botoes[0].innerHTML === botoes[2].innerHTML ||
+    var validacao = botoes[0].innerHTML !== "&nbsp;" && botoes[0].innerHTML === botoes[1].innerHTML && botoes[0].innerHTML === botoes[2].innerHTML ||
     botoes[3].innerHTML !== "&nbsp;" && botoes[3].innerHTML === botoes[4].innerHTML && botoes[3].innerHTML === botoes[5].innerHTML ||
     botoes[6].innerHTML !== "&nbsp;" && botoes[6].innerHTML === botoes[7].innerHTML && botoes[6].innerHTML === botoes[8].innerHTML ||
     botoes[0].innerHTML !== "&nbsp;" && botoes[0].innerHTML === botoes[3].innerHTML && botoes[0].innerHTML === botoes[6].innerHTML ||
@@ -43,7 +54,7 @@ function validarJogo() {
 }
 
 function resetar() {
-    const botoes = document.querySelectorAll(".tabuleiro .botao");
+    var botoes = document.querySelectorAll(".tabuleiro .botao");
     botoes.forEach(function(botao, index) {
         if(index < 9) {
             botao.innerHTML = "&nbsp";
@@ -52,4 +63,13 @@ function resetar() {
     jogador = "X";
     quantidadeJogadas = 0;
     jogoFinalizado = false;
+    atualizaPlacar();
+}
+
+function atualizaPlacar() {
+    var placarX = document.querySelector(".placarX");
+    var placarO = document.querySelector(".placarO");
+
+    placarX.innerHTML = "X = " + pontuacaoX;
+    placarO.innerHTML = "O = " + pontuacaoO;
 }
